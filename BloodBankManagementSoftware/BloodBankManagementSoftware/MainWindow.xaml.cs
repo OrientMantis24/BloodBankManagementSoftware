23f737e28bbeefc31ed73d298c40e36765900516
Bartosz Ociepka <bartosz.ociepka96@gmail.com>
2016-02-24 00:05:05 +0100

    Windows

---
+++ BloodBankManagementSoftware/BloodBankManagementSoftware/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BloodBankManagementSoftware
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //PnlMainWindow.MouseUp += new MouseButtonEventHandler(PnlMainWindow_MouseUp);
+        }
+
+        //private void PnlMainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        //}
+        private void Register_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Register OP = new Register();
+            //var host = new Window();
+            //host.Content = OP;
+            //.Show();
+            OP.Show();
+            //this.Close();
+        }
+    }
+}
